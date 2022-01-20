9ee7b3de929c07d8a8d0a8ecdc122f892046dab6
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-01-20 10:51:59 +0300

    feat: task brackets complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  
+  for(let i = 0; i < str.length; i++){
+    let indOpenBracket = bracketsConfig.findIndex(el => el[0] === str[i]);
+
+    if(indOpenBracket !== -1) {
+
+      if(bracketsConfig[indOpenBracket][1] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+
+    } else {
+
+      if(stack.length === 0) {
+        return false;
+      }
+
+      let indCloseBracket = bracketsConfig.findIndex(el => el[1] === str[i]);
+
+      if(bracketsConfig[indCloseBracket][0] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+
+    }
+    
+  }
+
+  return stack.length === 0;
+
 }
